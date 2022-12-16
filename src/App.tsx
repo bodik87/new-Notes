a94db8460fa79d9c90ba4e0fde52cba0e1ec8394
Bogdan <bsshul@gmail.com>
2022-12-17 00:50:23 +0200

    +

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TexturedCard } from "./components/TexturedCard";
 import { TodoCard } from "./components/TodoCard";
 
 import Menu from "./assets/oval.svg";
+import Plus from "./assets/plus.svg";
 
 function App() {
   return (
@@ -22,6 +23,12 @@ function App() {
           <TextCard />
           <TexturedCard />
         </div>
+      </div>
+      <div className=" w-[172px] h-[88px] fixed left-1/2 -translate-x-1/2 bottom-5 flex justify-center items-center rounded-full gap-1 bg-white/5 backdrop-blur-md z-40">
+        <div className=" w-20 h-20 flex justify-center items-center rounded-full bg-black backdrop-blur-md">
+          <img src={Plus} alt="fav" className="w-6 h-6" />
+        </div>
+        <div className=" w-20 h-20 flex justify-center items-center rounded-full bg-black/20 backdrop-blur-md"></div>
       </div>
     </div>
   );
