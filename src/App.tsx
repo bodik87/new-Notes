66e3dd95e9a977f943e18918120271a1956de458
Bogdan <bsshul@gmail.com>
2022-12-18 13:36:21 +0200

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Link, Outlet, useLocation } from "react-router-dom";
 import {
   categoriesRow,
   HOMEPAGE_TITLE,
@@ -14,79 +15,88 @@ import { AddButtons, MenuButton, Title } from "./components/UI";
 import { data } from "./data";
 
 function App() {
+  const location = useLocation();
   const foldersArray = data.filter((cart) => cart.type === "folder");
   const notesArray = data.filter((cart) => cart.type === "note");
 
   return (
-    <div className="bg-[url('./assets/bgDark.png')] w-full min-h-screen p-2 relative">
-      <div className="pt-2 mx-auto max-w-[408px] relative">
-        <MenuButton />
-        <Title title={HOMEPAGE_TITLE} />
+    <>
+      {location.pathname === "/" ? (
+        <div className="bg-[url('./assets/bgDark.png')] w-full min-h-screen p-2 relative">
+          <div className="pt-2 mx-auto max-w-[408px] relative">
+            <Outlet />
+            <MenuButton />
+            <Title title={HOMEPAGE_TITLE} />
 
-        <div className="flex gap-2 py-8 ml-4">
-          {categoriesRow.map((category, i) => (
-            <Category key={i} index={i} length={data.length}>
-              {category}
-            </Category>
-          ))}
+            <div className="flex gap-2 py-8 ml-4">
+              {categoriesRow.map((category, i) => (
+                <Category key={i} index={i} length={data.length}>
+                  {category}
+                </Category>
+              ))}
+            </div>
+
+            <div className="flex justify-center flex-wrap gap-2">
+              {foldersArray.map((folder) => (
+                <Link key={folder.id} to={`folder/${folder.id}`}>
+                  <Folder title={folder.title} isFavorite={folder.favorite} />
+                </Link>
+              ))}
+
+              {notesArray.map((note) => {
+                if (note.sections?.[0].sectionType === SECTIONS_TYPE.todo) {
+                  return (
+                    <Link key={note.id} to={`note/${note.id}`}>
+                      <TodoCard
+                        title={note.title}
+                        color={note.style?.color}
+                        isFavorite={note.favorite}
+                        todos={note.sections
+                          ?.filter(
+                            (section) =>
+                              section.sectionType === SECTIONS_TYPE.todo &&
+                              section.body
+                          )[0]
+                          .body.slice(0, 3)}
+                      />
+                    </Link>
+                  );
+                }
+                if (note.sections?.[0].sectionType === SECTIONS_TYPE.text) {
+                  return (
+                    <Link key={note.id} to={`note/${note.id}`}>
+                      <TextCard
+                        title={note.title}
+                        color={note.style?.color}
+                        isFavorite={note.favorite}
+                        text={note.sections[0].body}
+                      />
+                    </Link>
+                  );
+                }
+                if (note.sections?.[0].sectionType === SECTIONS_TYPE.image) {
+                  return (
+                    <Link key={note.id} to={`note/${note.id}`}>
+                      <TexturedCard
+                        title={note.title}
+                        color={note.style?.color}
+                        background={note.style?.background}
+                        image={note.sections[0].body[0]}
+                        isFavorite={note.favorite}
+                      />
+                    </Link>
+                  );
+                }
+              })}
+            </div>
+          </div>
+
+          <AddButtons />
         </div>
-
-        <div className="flex justify-center flex-wrap gap-2">
-          {foldersArray.map((folder) => (
-            <Folder
-              key={folder.id}
-              title={folder.title}
-              isFavorite={folder.favorite}
-            />
-          ))}
-
-          {notesArray.map((note) => {
-            if (note.sections?.[0].sectionType === SECTIONS_TYPE.todo) {
-              return (
-                <TodoCard
-                  key={note.id}
-                  title={note.title}
-                  color={note.style?.color}
-                  isFavorite={note.favorite}
-                  todos={note.sections
-                    ?.filter(
-                      (section) =>
-                        section.sectionType === SECTIONS_TYPE.todo &&
-                        section.body
-                    )[0]
-                    .body.slice(0, 3)}
-                />
-              );
-            }
-            if (note.sections?.[0].sectionType === SECTIONS_TYPE.text) {
-              return (
-                <TextCard
-                  key={note.id}
-                  title={note.title}
-                  color={note.style?.color}
-                  isFavorite={note.favorite}
-                  text={note.sections[0].body}
-                />
-              );
-            }
-            if (note.sections?.[0].sectionType === SECTIONS_TYPE.image) {
-              return (
-                <TexturedCard
-                  key={note.id}
-                  title={note.title}
-                  color={note.style?.color}
-                  background={note.style?.background}
-                  image={note.sections[0].body[0]}
-                  isFavorite={note.favorite}
-                />
-              );
-            }
-          })}
-        </div>
-      </div>
-
-      <AddButtons />
-    </div>
+      ) : (
+        <Outlet />
+      )}
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { FolderPage } from "./pages/FolderPage";
+import { NotePage } from "./pages/NotePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="folder/:folderId" element={<FolderPage />} />
+      <Route path="note/:noteId" element={<NotePage />} />
+      {/* <Route element={<AuthLayout />}>
+        <Route path="login" element={<Login />} loader={redirectIfUser} />
+        <Route path="logout" />
+        // loader={({ request }) =>
+        //   fetch("/api/dashboard.json", {
+        //     signal: request.signal,
+        //   })
+        // }
+      </Route> */}
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <RouterProvider router={router} />
 );
